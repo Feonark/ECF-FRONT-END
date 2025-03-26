3d0b6ae05893772f25c340a7c10a2a8be71956aa
Hugo <hugo.thivel@gmail.com>
2025-03-26 14:17:16 +0100

    commented Home

--- miamiam-ecffront-thivel/src/pages/Home/Home.jsx
+++ miamiam-ecffront-thivel/src/pages/Home/Home.jsx
@@ -22,13 +22,14 @@ function App() {
   const [showOnlyFav, setShowOnlyFav] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  // Ici je vais aller chercher à la fois les recettes stockées dans "recipes.json" et les recettes ajoutées en localStorage (s'il y en a)
   const getRecipes = async () => {
     try {
       const localRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
       const apiRecipes = await fetchRecipes();
-      const combinedRecipes = [...localRecipes, ...apiRecipes];
+      const combinedRecipes = [...localRecipes, ...apiRecipes]; // une fois récupérées je les combine
       setRecipes(combinedRecipes);
-      setFilteredRecipes(combinedRecipes);
+      setFilteredRecipes(combinedRecipes); // j'utilise filteredRecipes pour l'affichage étant donné que mes différents filtres (catégories de la recette, recherche et favoris) vont venir modifier le contenu de ce tableau
     } catch (error) {
       console.error("Error while loading recipes:", error);
     }
@@ -61,21 +62,25 @@ function App() {
     }
   };
 
+  // toggleFavorite va me permettre d'ajouter ou supprimer une recette des favoris (en localStorage)
   const toggleFavorite = (recipeId) => {
     let updatedFavorites;
     if (favorites.includes(recipeId)) {
-      updatedFavorites = favorites.filter((id) => id !== recipeId);
+      updatedFavorites = favorites.filter((id) => id !== recipeId); // Si mon tableau favorites contient l'id de la recette sur lequel je viens de cliquer le bouton favori, alors je filtre l'affichage pour que seules les recettes ne contenant pas cet id restent dans le tableau.
     } else {
-      updatedFavorites = [...favorites, recipeId];
+      updatedFavorites = [...favorites, recipeId]; // Sinon, j'ajoute l'id de la recette dans le tableau.
     }
-    setFavorites(updatedFavorites);
-    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    setFavorites(updatedFavorites); // J'actualise l'état du tableau des favoris
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites)); // J'actualise le localStorage pour qu'il intègre mes nouvelles données
   };
 
+  // Simple toggle pour l'ouverture de la modale. Il s'agit juste d'inverser l'état
   const toggleModal = () => {
     setIsModalOpen((prev) => !prev);
   };
 
+  // La fonction applyFilters va me permettre de filtrer mes recettes. J'ai 4 types de filtres interconnectés : favoris, origine de la recette, type et valeur de la recherche.
+  // Mon but c'est que les recettes soient filtrées selon ces critères simultanément
   const applyFilters = () => {
     let filteredData = recipes;
     if (showOnlyFav) {
@@ -99,6 +104,7 @@ function App() {
     setFilteredRecipes(filteredData);
   };
 
+  // Ce useEffect me permet de récupérer les données nécessaires à l'affichage de mes données + la récupération des favoris en localStorage à chaque chargement de la page (et seulement une fois)
   useEffect(() => {
     getRecipes();
     getOriginCategories();
@@ -110,10 +116,12 @@ function App() {
     }
   }, []);
 
+  // Ce useEffect se déclenche seulement lorsque les états mentionnés changent. Il va me permettre d'actualiser en temps réel l'affichage des recettes lorsque l'utilisateur les filtrent
   useEffect(() => {
     applyFilters();
   }, [selectedOriginCat, selectedTypeCat, searchValue, recipes, showOnlyFav]);
 
+  // Et enfin celui-là me permet de bloquer le scroll de la page Home lorsque la modale est ouverte.
   useEffect(() => {
     if (isModalOpen) {
       document.body.style.overflow = "hidden";
